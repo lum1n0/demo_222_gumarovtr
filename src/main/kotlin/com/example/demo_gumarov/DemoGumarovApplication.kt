e73217e6232504c7e88baf147895f6b110eb57fa
lum1n0 <qwpomnzx5555@gmail.com>
2026-05-19 09:42:10 +0300

    init commit

---
+++ src/main/kotlin/com/example/demo_gumarov/DemoGumarovApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo_gumarov
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoGumarovApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoGumarovApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo_gumarov/DemoGumarovApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo_gumarov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoGumarovApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
